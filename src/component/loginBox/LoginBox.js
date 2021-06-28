bc593003e388920d70e63e3bcca2d6c3621b77e1
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-06-28 10:48:36 +0900

    [component] 로그인 박스 작성 #9

---
+++ src/component/loginBox/LoginBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LoginBox() {
+    return (
+        <div>
+            <label>ID : </label>
+            <input type="text"></input>
+            <label>Password : </label>
+            <input type="text"> </input>
+        </div>
+    );
+}
+
+export default LoginBox;
