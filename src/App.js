af03c6758fcf14c01d75d756dbf80aac8cfa2983
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-06-28 14:31:56 +0900

    [Library] Add react-router-dom #35

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
+import Routes from "./Routes"
+import './App.css'
 
-import React from 'react'
-import LoginPage from "./domain/login/LoginPage";
-import BlogPage from "./domain/blog/BlogPage";
+
+// 기능은 대부분 해당 유저의 seq 값으로 움직이도록 설계
+
+// 바디 클릭시 팝업되어 있던 모든 특정 스타일(시간, 팝업 등)이 지워지도록 작용.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <LoginPage></LoginPage>
-      <BlogPage></BlogPage>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Routes></Routes>
+        </div>
+    );
 }
 
 export default App;
