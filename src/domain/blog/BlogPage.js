1ff7a820a1095bb4ceda15dc6125cb78b9917600
askePhoenix <73146032+askePhoenix@users.noreply.github.com>
2021-07-02 14:23:35 +0900

    Develop (#44)
    * [component] 로그인 박스 작성 #9 (#30)
    
    * Develop (#8)
    
    * React 빌드 완료 #1
    
    * Add, React 빌드를 위한 Circle Ci #1
    
    * Add .circleci/config.yml
    
    * edit Circle Ci Config #build-and-test
    
    * edit Circle Ci Config #build-and-test
    
    * edit Circle Ci Config #build-and-test
    
    * Feature/4 add jest (#5)
    
    * Add Jest Code #4
    
    * edit run script #4
    
    Co-authored-by: askeundaia <48040020+askeundaia@users.noreply.github.com>
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * add test code coverage
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Update README.md
    
    * Update issue templates Component
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update README.md
    
    * Update README.md
    
    * Update issue templates
    
    * [component] 로그인 박스 작성 #9
    
    * [PR]공백 제거 #9
    
    * Add .circleci/config.yml
    
    * Add .circleci/config.yml
    
    Co-authored-by: askeundaia <48040020+askeundaia@users.noreply.github.com>
    
    * [domain] 블로그 페이지 작성 #14 (#32)
    
    * [Library] Add react-router-dom #33 (#34)
    
    * Feature/35 add router (#36)
    
    * [Library] Add react-router-dom #35
    
    * [Library] Add react-router-dom #35
    
    - [x] 1. App.js 삭제
    - [x] 2. App.test.js 삭제
    - [x] 3. App.test.js.snap 삭제
    
    * Add material-ui/core #37
    
    * Feature/11 add loginbox (#38)
    
    * Develop (#8)
    
    * React 빌드 완료 #1
    
    * Add, React 빌드를 위한 Circle Ci #1
    
    * Add .circleci/config.yml
    
    * edit Circle Ci Config #build-and-test
    
    * edit Circle Ci Config #build-and-test
    
    * edit Circle Ci Config #build-and-test
    
    * Feature/4 add jest (#5)
    
    * Add Jest Code #4
    
    * edit run script #4
    
    Co-authored-by: askeundaia <48040020+askeundaia@users.noreply.github.com>
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * add test code coverage
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Update README.md
    
    * Update issue templates Component
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * Update README.md
    
    * Update README.md
    
    * Update issue templates
    
    * Add LoginBox in LoginPage #11
    
    * Add material-ui/core #11
    
    + yarn add @material-ui/icons
    + 로그인 템플릿 샘플 적용
    
    * Add .circleci/config.yml
    
    * 테스트 #11
    
    * Add .circleci/config.yml
    
    * Updated config.yml
    
    Co-authored-by: askeundaia <48040020+askeundaia@users.noreply.github.com>
    
    * edit Favicon #41
    
    * Demo 작업 #41
    
    * Demo 작업 불필요 코드 삭제 #41
    
    * edit Link & URL #41 (#43)
    
    Co-authored-by: askeundaia <48040020+askeundaia@users.noreply.github.com>

--- src/domain/blog/BlogPage.js
+++ src/domain/blog/BlogPage.js
@@ -1,14 +1,58 @@
-import React from 'react'
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Header from "../../component/header/Header";
+import Copyright from "../../component/copyrignt/Copyright";
 
-function BlogPage() {
+
+const sections = [
+    {title: 'Python', url: '#'},
+    {title: 'Javascript', url: '#'},
+    {title: 'C lang', url: '#'},
+    {title: 'Go lang', url: '#'},
+    {title: 'Java', url: '#'},
+    {title: 'Swift', url: '#'},
+    {title: 'C++', url: '#'},
+    {title: 'React', url: '#'},
+    {title: 'Android', url: '#'},
+    {title: '추가', url: '#'},
+];
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function BlogPage() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <label>ID : </label>
-            <input type="text"></input>
-            <label>Password : </label>
-            <input type="text"></input>
-        </div>
+        <Container component="main">
+            <Header title="Blog" sections={sections}> </Header>
+            <CssBaseline/>
+            <div className={classes.paper}>
+                컨텐츠
+            </div>
+            <Box mt={8}>
+                <Copyright/>
+            </Box>
+        </Container>
     );
 }
-
-export default BlogPage;
